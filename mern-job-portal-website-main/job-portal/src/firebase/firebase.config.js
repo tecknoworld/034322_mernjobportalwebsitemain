542dfb58ca4e6ed140d5a15d45f051d47c404aa6
tecknoworld <ofentselebogo@gmail.com>
2024-04-10 16:00:59 +0200

    first commit

---
+++ mern-job-portal-website-main/job-portal/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDDpx0bowjvpFx6iUxfJpdKgVEQZH64Sk",
+  authDomain: "job-portal-demo-f0af3.firebaseapp.com",
+  projectId: "job-portal-demo-f0af3",
+  storageBucket: "job-portal-demo-f0af3.appspot.com",
+  messagingSenderId: "163410296623",
+  appId: "1:163410296623:web:00861128fb0a1d9816abda"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
